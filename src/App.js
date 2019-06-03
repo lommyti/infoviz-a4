352be0d3bdc7aa3e19a3ae60b80fb5a6212c8478
Jodie Wei <jodiewei2019@u.northwestern.edu>
2019-06-03 17:13:42 -0500

    react init

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="step">
+        <h1>STEP 1</h1>
+        <p>combine sugar and butter.</p>
+      </div>
+      <div className="step">
+        <h1>STEP 2</h1>
+        <p>add egg and vanilla.</p>
+      </div>
+      <div className="step">
+        <h1>STEP 3</h1>
+        <p>combine flour, salt, baking soda.</p>
+      </div>
+      <div className="step">
+        <h1>STEP 4</h1>
+        <p>combine wet and dry ingredients, and fold in chocolate chips.</p>
+      </div>
+      <div className="step">
+        <h1>STEP 4</h1>
+        <p>bake.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
