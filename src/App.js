2faca4a9d378809f2cba852c51788c52d2d565db
lommyti <thomasli2020@u.northwestern.edu>
2019-06-05 21:07:46 -0500

    dragging works on my computer

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ import eggVanillaImg from './img/egg_vanilla.png';
 import dryIngrededientsImg from './img/dry_ingredients.png';
 import chocChipsImg from './img/chocolate_chips.png';
 import cookieTrayImg from './img/cookie_tray.png';
+import trayImg from './img/tray.png';
 import emptyBowlImg from './img/bowl.png';
 import butterSugarBowlImg from './img/butter_sugar_bowl.png';
 import eggVanillaBowlImg from './img/egg_vanilla_bowl.png';
 import dryIngBowlImg from './img/dry_ingredients_bowl.png';
 import ovenImg from './img/oven.png';
 import cookieImg from './img/cookie.png';
+import chocChipsBowlImg from './img/chocolate_chips_bowl.png'
 
 
 
@@ -26,6 +28,9 @@ class App extends React.Component {
      this.state = {
         selected_ingred: null,
         texture_change_text: null,
+        img: emptyBowlImg,
+        trayImg: trayImg,
+        draggedObj: null,
      };
    }
 
@@ -49,10 +54,54 @@ class App extends React.Component {
   setTextureCakey = () => {
     this.setState({texture_change_text: "For cakey cookies..."})
   }
+
+  onDrag = (event, dragVal) => {
+  event.preventDefault();
+  this.setState({
+    draggedObj: dragVal,
+  });
+}
+
+onDragOver = (event) => {
+  event.preventDefault();
+}
+
+onDrop = (event) => {
+  if(this.state.draggedObj == 1) {
+  this.setState({
+    img: butterSugarBowlImg,
+  });
+}
+else if(this.state.draggedObj == 2) {
+this.setState({
+  img: eggVanillaBowlImg,
+});
+}
+else if(this.state.draggedObj == 3) {
+this.setState({
+  img: dryIngBowlImg,
+});
+}
+
+else if(this.state.draggedObj == 4) {
+this.setState({
+  img: chocChipsBowlImg,
+});
+}
+
+else if(this.state.draggedObj == 5) {
+this.setState({
+  img:emptyBowlImg,
+  trayImg: cookieTrayImg,
+});
+}
+}
+
 render(){
 
   return (
      <div className="w-h-100">
+
         <div className="App-header">
         <h1>What Makes a Perfect Cookie?</h1>
         <h2>Thomas Li, Ilham Nurjadin, Jodie Wei &middot;<br/> EECS 396: Interactive Information Visualization</h2>
@@ -60,6 +109,7 @@ render(){
         <div className="divider-brown"><div className="divider-brown-inside"></div></div>
         <div className="bar-brown"></div>
         <div className="App">
+        <ReactTooltip multiline={true} place="right" type="dark" effect="float"/>
           <p className="intro">
             Everyone has a favorite kind of chocolate chip cookie, from wafer-thin and crispy to thick and cakey.
             Though cookies come in a variety of different textures, they often borm from the same ingredients and general process.
@@ -70,8 +120,13 @@ render(){
             generalized steps that go into making chocolate chip cookies. Then we'll talk about some of the most popular textures and the changes
             from an ordinary cookie that are needed to take yours to the next level!<br/><br/>
             To learn more about each ingredient, hover over any of the <b data-tip="And you'll see info here!">underlined ingredients</b> and
-            a tooltip will appear that explains more information about how that particular ingredient impacts the baking process.
+            a tooltip will appear that explains more information about how that particular ingredient impacts the baking process.<br/><br/>
+            Look to your right! We even got you a bowl.
           </p>
+          <div className="bowl">
+            <img src={this.state.img} style={{width: '25%'}} alt='bowl' onDrop={event => this.onDrop(event)}
+       onDragOver={(event => this.onDragOver(event))}draggable="true" onDrag={(event) => this.onDrag(event, 5)}/>
+           </div>
           <div className="entire_step">
             <div className="step">
               <h1>STEP 1</h1>
@@ -86,15 +141,14 @@ render(){
                           Mixing room-temperature butter and sugar creates air pockets, for a puffier cookie.<br/>
                           The fat in butter helps prevent gluten from forming, so using high-fat butter will make the cookie less chewy.<br/>
                           Substituting with margarine or shortening will make the cookie spread less."> butter </b>
-              together in a bowl.</p>
+              together by dragging them to the bowl.</p>
             </div>
 
             <div className="images">
               <div className="drag_images">
-                <img src={butterSugarImg} style={{height: '20em'}} alt='butter'/>
+                <img src={butterSugarImg} style={{height: '20em'}} alt='butter' draggable="true" onDrag={(event) => this.onDrag(event, 1)}/>
               </div>
               <div className="static_images">
-                <img src={emptyBowlImg} style={{height: '30em'}} alt='bowl'/>
               </div>
             </div>
           </div>
@@ -113,10 +167,7 @@ render(){
 
               <div className="images">
                 <div className="drag_images">
-                  <img src={eggVanillaImg} style={{height: '20em'}} alt='butter'/>
-                </div>
-                <div className="static_images">
-                  <img src={butterSugarBowlImg} style={{height: '30em'}} alt='bowl'/>
+                  <img src={eggVanillaImg} style={{height: '20em'}} alt='butter'draggable="true" onDrag={(event) => this.onDrag(event, 2)}/>
                 </div>
               </div>
             </div>
@@ -139,10 +190,9 @@ render(){
 
             <div className="images">
               <div className="drag_images">
-                <img src={dryIngrededientsImg} style={{height: '20em'}} alt='butter'/>
+                <img src={dryIngrededientsImg} style={{height: '20em'}} alt='butter' draggable="true" onDrag={(event) => this.onDrag(event, 3)}/>
               </div>
               <div className="static_images">
-                <img src={dryIngBowlImg} style={{height: '30em'}} alt='bowl'/>
               </div>
             </div>
           </div>
@@ -159,10 +209,9 @@ render(){
 
             <div className="images">
               <div className="drag_images">
-                <img src={chocChipsImg} style={{height: '20em'}} alt='butter'/>
+                <img src={chocChipsImg} style={{height: '20em'}} alt='butter' draggable="true" onDrag={(event) => this.onDrag(event, 4)}/>
               </div>
               <div className="static_images">
-                <img src={dryIngBowlImg} style={{height: '30em'}} alt='bowl'/>
               </div>
             </div>
           </div>
@@ -184,35 +233,40 @@ render(){
 
             <div className="images">
               <div className="drag_images">
-                  <img src={cookieTrayImg} style={{height: '20em'}} alt='cookie tray'/>
+                  <img src={this.state.trayImg} style={{height: '20em'}} alt='cookie tray'  onDrop={event => this.onDrop(event)}
+             onDragOver={(event => this.onDragOver(event))}/>
                 </div>
-              <div className="static_images">
-                  <img src={ovenImg} style={{height: '30em'}} alt='oven'/>
-              </div>
             </div>
           </div>
-          <p>And after all of that hard work, your cookies are done!</p><br/><br/>
-          <div className="center-text">
-            <img src={cookieImg} style={{height: '20em'}} alt='cookie'/>
-            <img src={cookieImg} style={{height: '20em'}} alt='cookie'/>
-            <img src={cookieImg} style={{height: '20em'}} alt='cookie'/>
+          <div className="lastp">
+          <p>And after all of that hard work, your cookies are done!</p></div>
 
-          </div>
-          <p>This, of course, is a little simpler than what <em>actually</em> goes on; we didn't even give you the proportions of the ingredients you were using!
-          Truthfully, there is no true baseline cookie that we can use as the gold standard here.
-          However, numerous sources have done tests to try and maximize the effects of these ingredients and create unique textures; we've compiled some of the best
-          we saw and tried to figure out what made each of them unique in terms of the relative ingredient proportions.
-          <br/><br/>
-          Interested in switching up your cookie? Do you want it to be... </p><br/><br/>
-          <div className="center-text">
-          <button className="texture-button" onClick={this.setTextureCrunchy}>Crunchy</button>
-          <button className="texture-button" onClick={this.setTextureChewy}>Chewy</button>
-          <button className="texture-button" onClick={this.setTextureCakey}>Cakey</button>
-          </div>
-          <br/><br/>
-          <p>{this.state.texture_change_text}</p>
         </div>
-        <ReactTooltip multiline={true} place="right" type="dark" effect="float"/>
+        <div className="oven">
+            <img src={ovenImg} style={{height: '30em'}} alt='oven'/>
+        </div>
+        <div className="other"><p>And after all of that hard work, your cookies are done!</p>
+                <br/><br/>
+                <div class="center-text">
+                <img src={cookieImg} style={{height: '20em'}} alt='cookie'/>
+                <img src={cookieImg} style={{height: '20em'}} alt='cookie'/>
+                <img src={cookieImg} style={{height: '20em'}} alt='cookie'/>
+                </div>
+                  <p>This, of course, is a little simpler than what <em>actually</em> goes on; we didn't even give you the proportions of the ingredients you were using!
+                  Truthfully, there is no true baseline cookie that we can use as the gold standard here.
+                  However, numerous sources have done tests to try and maximize the effects of these ingredients and create unique textures; we've compiled some of the best
+                  we saw and tried to figure out what made each of them unique in terms of the relative ingredient proportions.
+                  <br/><br/>
+                  Interested in switching up your cookie? Do you want it to be... </p><br/><br/>
+                  <div className="center-text">
+                  <button className="texture-button" onClick={this.setTextureCrunchy}>Crunchy</button>
+                  <button className="texture-button" onClick={this.setTextureChewy}>Chewy</button>
+                  <button className="texture-button" onClick={this.setTextureCakey}>Cakey</button>
+                  </div>
+                  <br/><br/>
+                  <p>{this.state.texture_change_text}</p>
+                  </div>
+
       </div>
   );
 }
