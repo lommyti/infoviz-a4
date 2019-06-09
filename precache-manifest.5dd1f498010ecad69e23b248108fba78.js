092415f8f42427507c64a82cc85b068c8fbb7d03
lommyti <thomasli2020@u.northwestern.edu>
2019-06-09 18:32:14 -0500

    Updates

---
+++ precache-manifest.5dd1f498010ecad69e23b248108fba78.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397838d8a414d5b352a406f87d35b614",
+    "url": "/infoviz-a4/index.html"
+  },
+  {
+    "revision": "e305a5f7c5f97a3379a7",
+    "url": "/infoviz-a4/static/css/main.9a6279c8.chunk.css"
+  },
+  {
+    "revision": "600ad508a6aecdadf2ca",
+    "url": "/infoviz-a4/static/js/2.4374101f.chunk.js"
+  },
+  {
+    "revision": "e305a5f7c5f97a3379a7",
+    "url": "/infoviz-a4/static/js/main.b0fd3a6e.chunk.js"
+  },
+  {
+    "revision": "0fb23507a22c4f600e5c",
+    "url": "/infoviz-a4/static/js/runtime~main.19a0dda0.js"
+  },
+  {
+    "revision": "82f629c01343d84338f3e3c7d8a82686",
+    "url": "/infoviz-a4/static/media/bowl.82f629c0.png"
+  },
+  {
+    "revision": "7341d326c48c0fc4eec87136729bd9e7",
+    "url": "/infoviz-a4/static/media/butter_sugar.7341d326.png"
+  },
+  {
+    "revision": "d831f849532de6a62ed7a2ae718f9607",
+    "url": "/infoviz-a4/static/media/butter_sugar_bowl.d831f849.png"
+  },
+  {
+    "revision": "0cfab0fd39fbc435ad45db559f376c07",
+    "url": "/infoviz-a4/static/media/chocolate_chips.0cfab0fd.png"
+  },
+  {
+    "revision": "fe364a645798102a2fea1977456576af",
+    "url": "/infoviz-a4/static/media/chocolate_chips_bowl.fe364a64.png"
+  },
+  {
+    "revision": "31a004dd9bcb3571eb4e031ff8480bb4",
+    "url": "/infoviz-a4/static/media/cookie.31a004dd.png"
+  },
+  {
+    "revision": "808ffbfe46887017dd68e3408bccc845",
+    "url": "/infoviz-a4/static/media/cookie_side_view.808ffbfe.png"
+  },
+  {
+    "revision": "91b8101ba8ef70ec3f557281f05df8b4",
+    "url": "/infoviz-a4/static/media/cookie_tray.91b8101b.png"
+  },
+  {
+    "revision": "5966224fa0ede36dd88195bc1b72c90f",
+    "url": "/infoviz-a4/static/media/dry_ingredients.5966224f.png"
+  },
+  {
+    "revision": "971f7bd5ce767a5676c1ded1b8d91841",
+    "url": "/infoviz-a4/static/media/dry_ingredients_bowl.971f7bd5.png"
+  },
+  {
+    "revision": "eb5d3ccf134741bbff0e52bb167eee1d",
+    "url": "/infoviz-a4/static/media/egg_vanilla.eb5d3ccf.png"
+  },
+  {
+    "revision": "9d3037a0b1623176d8363b734b177415",
+    "url": "/infoviz-a4/static/media/egg_vanilla_bowl.9d3037a0.png"
+  },
+  {
+    "revision": "4e2efbd8ed2f805ea8ba70e835fc8366",
+    "url": "/infoviz-a4/static/media/oven.4e2efbd8.png"
+  },
+  {
+    "revision": "f061ec42ba13dc97856d9df685b8b169",
+    "url": "/infoviz-a4/static/media/oven.f061ec42.gif"
+  },
+  {
+    "revision": "879b37592310e12b69a5343e9ce2e9db",
+    "url": "/infoviz-a4/static/media/tray.879b3759.png"
+  }
+]);
