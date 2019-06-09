fb03d0f72e58fa6377892b61831fa5deb9a657b2
Jodie Wei <jodiewei2019@u.northwestern.edu>
2019-06-09 13:15:00 -0500

    skeleton for customization

---
+++ src/Customization.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+
+class Customization extends React.Component {
+    constructor(props) {
+     super(props);
+     this.state = {
+     };
+   }
+
+ 
+
+render(){
+
+  return (
+  );
+}
+}
+
+export default Customization;
